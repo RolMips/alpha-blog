dff3bdf68ce3125edb0c47b032dc9dcb2e45c808
Roland Martinet <r.martinet@nerya.fr>
2024-02-29 13:48:12 +0100

    edit_users_(142)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-    before_action :set_article, only: [:show, :edit, :edit, :update, :destroy]
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def index
         @articles = Article.all
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         @article.user = User.first
         if @article.save
-            flash[:notice] = "Article was created succesfully."
+            flash[:notice] = "Article was succesfully created."
             redirect_to @article
         else
             render :new
@@ -29,16 +29,16 @@ class ArticlesController < ApplicationController
 
     def update
         if @article.update(article_params)
-            flash[:notice] = "Article was updated succesfully."
+            flash[:notice] = "Article was succesfully updated."
             redirect_to @article
         else
-            render 'edit'
+            render :edit
         end
     end
 
     def destroy
         @article.destroy
-        flash[:notice] = "Article was deleted succesfully."
+        flash[:notice] = "Article was succesfully deleted."
         redirect_to articles_path
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update]
+
 def new
   @user = User.new
 end
@@ -14,10 +16,26 @@ def create
   end
 end
 
+def edit
+end
+
+def update
+  if @user.update(user_params)
+    flash[:notice] = "Your account was succesfully updated."
+    redirect_to articles_path
+else
+    render :edit
+end
+end
+
 private
 
 def user_params
     params.require(:user).permit(:username, :email, :password)
+end
+
+def set_user
+  @user = User.find(params[:id])
 end
 
 end
