4403056fb3a4fb26442133d85e64661ab56bcd97
Roland Martinet <r.martinet@nerya.fr>
2024-03-07 17:14:19 +0100

    authentication_helper_methods_(157)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:edit, :update, :show]
+before_action :set_user, only: [:show, :edit, :update]
 
 def new
   @user = User.new
@@ -9,6 +9,7 @@ end
 def create
   @user = User.new(user_params)
   if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog ! Your have succesfully sign up!"
       redirect_to articles_path
   else
